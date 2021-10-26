3eeebeb9202fd0e53712a8499c5f3ae016642493
Raznan <muhammadraznan@satpaminternet.com>
2021-10-26 13:16:33 +0700

    Pressing CTRL will prevent scrolling on GraphEdit

--- DialogueGraph.gd
+++ DialogueGraph.gd
@@ -1,5 +1,26 @@
 extends GraphEdit
 
+onready var _previous_scroll_offset := scroll_offset
+
 func _ready() -> void:
-	print_debug(get_children()[0].get_children())
-	print_debug(get_zoom_hbox().get_children())
+	for child in get_graphfilter():
+		if child is ScrollBar:
+			child.connect("value_changed", self, "_on_%s_value_changed" % child.get_class(), [child])
+
+
+func _input(event: InputEvent) -> void:
+	_previous_scroll_offset = scroll_offset
+	
+	
+func get_graphfilter() -> Control:
+	return get_children()[0].get_children()
+	
+	
+func _on_HScrollBar_value_changed(value: float, scrollbar : ScrollBar) -> void:
+	if Input.is_key_pressed(KEY_CONTROL):
+		scrollbar.value = _previous_scroll_offset.x 
+	
+	
+func _on_VScrollBar_value_changed(value: float, scrollbar : ScrollBar) -> void:
+	if Input.is_key_pressed(KEY_CONTROL):
+		scrollbar.value = _previous_scroll_offset.y
