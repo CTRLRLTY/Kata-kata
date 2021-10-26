bf53ebc3399e27ab637a18ef5774acd416fd028f
Raznan <muhammadraznan@satpaminternet.com>
2021-10-26 18:01:18 +0700

    Added zoom functionality when pressing CTRL+MOUSE_SCROLL

--- DialogueGraph.gd
+++ DialogueGraph.gd
@@ -11,11 +11,23 @@ func _ready() -> void:
 func _input(event: InputEvent) -> void:
 	_previous_scroll_offset = scroll_offset
 	
+	if event is InputEventMouseButton:
+		if not event.control:
+			return
+			
+		if not event.is_pressed():
+			return
+			
+		if event.button_index == BUTTON_WHEEL_UP:
+			zoom *= 1.2
+		elif event.button_index == BUTTON_WHEEL_DOWN:
+			zoom /= 1.2
+		
 	
 func get_graphfilter() -> Control:
 	return get_children()[0].get_children()
-	
-	
+
+
 func _on_HScrollBar_value_changed(value: float, scrollbar : ScrollBar) -> void:
 	if Input.is_key_pressed(KEY_CONTROL):
 		scrollbar.value = _previous_scroll_offset.x 
