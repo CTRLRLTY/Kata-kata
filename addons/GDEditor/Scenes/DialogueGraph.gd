7dba0ed8e3245e7b92ff3c5f72efb8e7a112c1e0
Raznan <muhammadraznan@satpaminternet.com>
2021-12-11 20:35:49 +0700

    Removed makeshift zoom logic, since 3.4 already included a built-in solution

--- addons/GDEditor/Scenes/DialogueGraph.gd
+++ addons/GDEditor/Scenes/DialogueGraph.gd
@@ -2,32 +2,7 @@ tool
 
 extends GraphEdit
 
-var _previous_scroll_offset := scroll_offset
-var scrollbar := {}
 
-func _enter_tree() -> void:
-	for child in get_graphfilter():
-		if child is ScrollBar:
-			scrollbar[child.get_class().to_lower()] = child
-			child.connect("value_changed", self, "_on_%s_value_changed" % child.get_class(), [child])
-
-
-func _gui_input(event: InputEvent) -> void:
-	_previous_scroll_offset = scroll_offset
-	
-	if event is InputEventMouseButton:
-		if not event.control:
-			return
-			
-		if not event.is_pressed():
-			return
-
-		if event.button_index == BUTTON_WHEEL_UP:
-			zoom *= 1.2
-		elif event.button_index == BUTTON_WHEEL_DOWN:
-			zoom /= 1.2
-			
-		
 func can_drop_data(_position: Vector2, data) -> bool:
 	return data is Dictionary and data.get("value_type", "") == "GDComponent" 
 	
@@ -38,17 +13,3 @@ func drop_data(position: Vector2, data : Dictionary) -> void:
 	add_child(gn)
 	gn.owner = owner
 	gn.offset = position + scroll_offset
-	
-
-func get_graphfilter() -> Control:
-	return get_children()[0].get_children()
-	
-
-func _on_HScrollBar_value_changed(value: float, scrollbar : ScrollBar) -> void:
-	if Input.is_key_pressed(KEY_CONTROL):
-		scrollbar.value = _previous_scroll_offset.x 
-
-
-func _on_VScrollBar_value_changed(value: float, scrollbar : ScrollBar) -> void:
-	if Input.is_key_pressed(KEY_CONTROL):
-		scrollbar.value = _previous_scroll_offset.y
