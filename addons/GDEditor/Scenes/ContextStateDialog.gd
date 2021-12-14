75b57806a924cfa793b9d6c7fc741fa4c83f3732
Raznan <muhammadraznan@satpaminternet.com>
2021-12-14 23:04:49 +0700

    Added Value state Array resizing

--- addons/GDEditor/Scenes/ContextStateDialog.gd
+++ addons/GDEditor/Scenes/ContextStateDialog.gd
@@ -5,47 +5,71 @@ extends WindowDialog
 signal confirmed
 
 var type_option : OptionButton
+var array_check : CheckBox
+var array_size : SpinBox
+var array_dialog : WindowDialog
 var value_edit : LineEdit
 var value_check : CheckBox
+var value_array : Button
 
 var state_data : ContextStateData
 
 
 func _enter_tree() -> void:
 	type_option = find_node("TypeOption")
+	
 	value_edit = find_node("ValueEdit")
 	value_check = find_node("ValueCheck")
+	value_array = find_node("ValueArray")
+	
+	array_check = find_node("ArrayCheck")
+	array_size = find_node("ArraySize")
+	array_dialog = value_array.get_node("ValueArrayDialog")
+
 	
 	if not state_data:
 		state_data = ContextStateData.new()
+		
+	_update_value_inputnode(type_option.get_selected_id())
+
+
+func _update_value_inputnode(state_type : int, is_array := false) -> void:
+	if not is_array:
+		value_array.hide()
+		
+		match state_type:
+			ContextStateData.TYPE_STRING:
+				value_edit.show()
+				value_check.hide()
+				value_edit.text = ""
+				
+			ContextStateData.TYPE_BOOL:
+				value_edit.hide()
+				value_check.show()
+				value_check.pressed = false
+				
+			_:
+				value_edit.show()
+				value_check.hide()
+				value_edit.text = "0"
+	else:
+		value_edit.hide()
+		value_check.hide()
+		value_array.show()
 	
 
 func _on_TypeOption_item_selected(index: int) -> void:
 	state_data.state_type = type_option.get_item_id(index)
-	match state_data.state_type:
-		ContextStateData.TYPE_STRING:
-			value_edit.show()
-			value_check.hide()
-			value_edit.text = ""
-			state_data.state_value = value_edit.text
-			
-		ContextStateData.TYPE_BOOL:
-			value_edit.hide()
-			value_check.show()
-			value_check.pressed = false
-			state_data.state_value = ""
-			
-		ContextStateData.TYPE_INT:
-			value_edit.show()
-			value_check.hide()
-			value_edit.text = "0"
-			state_data.state_value = value_edit.text
-			
-		ContextStateData.TYPE_FLOAT:
-			value_edit.show()
-			value_check.hide()
-			value_edit.text = "0"
-			state_data.state_value = value_edit.text
+	_update_value_inputnode(state_data.state_type, array_check.pressed)
+
+
+func _on_ArrayCheck_toggled(button_pressed: bool) -> void:
+	_update_value_inputnode(state_data.state_type, button_pressed)
+	array_size.editable = button_pressed
+
+
+func _on_ArraySize_value_changed(value: float) -> void:
+	array_dialog.resize(value)
 
 
 func _on_ValueEdit_focus_exited() -> void:
@@ -68,6 +92,10 @@ func _on_ValueCheck_toggled(button_pressed: bool) -> void:
 		state_data.state_value = ""
 
 
+func _on_ValueArray_pressed() -> void:
+	array_dialog.popup_centered()
+
+
 func _on_CancelBtn_pressed() -> void:
 	hide()
 
@@ -75,4 +103,3 @@ func _on_CancelBtn_pressed() -> void:
 func _on_ConfirmBtn_pressed() -> void:
 	hide()
 	emit_signal("confirmed")
-
---
+++ addons/GDEditor/Scenes/ValueArrayDialog.gd
@@ -0,0 +1,84 @@
+tool
+
+extends WindowDialog
+
+var item_container : Container
+
+var value_list : PoolStringArray
+
+
+func _enter_tree() -> void:
+	item_container = find_node("ItemContainer")
+	
+	if not value_list:
+		value_list = PoolStringArray()
+		
+	add_item()
+
+
+func _exit_tree() -> void:
+	clear()
+
+
+func resize(num : int) -> void:
+	# Array is growing
+	if value_list.size() > num:
+		for _i in range(num):
+			pop_item()
+	# Array list shrinking
+	elif value_list.size() < num:
+		for _i in range(num - value_list.size()):
+			add_item()
+	
+
+
+func add_item(value := "") -> void:
+	var item := PanelContainer.new()
+	var hbox = HBoxContainer.new()
+	var drag_btn := ToolButton.new()
+	var index_label := Label.new()
+	var value_edit := LineEdit.new()
+	
+	drag_btn.icon = load("res://addons/GDEditor/Resources/Icons/TripleBar.png")
+	index_label.text = str(value_list.size())
+	index_label.align = Label.ALIGN_CENTER
+	index_label.valign = Label.VALIGN_CENTER
+	index_label.size_flags_horizontal += SIZE_EXPAND 
+	index_label.size_flags_stretch_ratio = 0.4
+	
+	value_edit.text = value
+	value_edit.size_flags_horizontal += SIZE_EXPAND
+	value_edit.connect("text_changed", self, "_on_value_edit_changed", [item])
+	
+	# Expand the list
+	value_list.append("")
+	
+	hbox.add_child(drag_btn)
+	hbox.add_child(index_label)
+	hbox.add_child(value_edit)
+	item.add_child(hbox)
+	
+	item_container.add_child(item)
+
+
+func pop_item() -> void:
+	var last_child : Control = item_container.get_child(item_container.get_child_count() - 1)
+	
+	# Make sure last pop invocation finishes, otherwise skip waiting.
+	if last_child.is_queued_for_deletion():
+		yield(get_tree(), "idle_frame")
+		pop_item()
+	else:
+		last_child.queue_free()
+		value_list.remove(last_child.get_position_in_parent())
+
+
+func clear() -> void:
+	for item in item_container.get_children():
+		item.queue_free()
+
+
+func _on_value_edit_changed(new_value : String, item : Container) -> void:
+	assert(value_list.size() - 1 >= item.get_position_in_parent())
+
+	value_list[item.get_position_in_parent()] = new_value
