b40068c167ee54671015ade84a3728b48ddfd0e3
Raznan <muhammadraznan@satpaminternet.com>
2021-12-19 09:39:08 +0700

    Removed Node suffix

--- addons/GDEditor/Scenes/NodeSelection.gd
+++ addons/GDEditor/Scenes/NodeSelection.gd
@@ -5,7 +5,7 @@ extends ItemList
 func get_drag_data(position: Vector2):
 	var selected_id := get_item_at_position(position)
 	var selected_text := get_item_text(selected_id)
-	var p : PackedScene = load(GDUtil.resolve(selected_text + "Node.tscn"))
+	var p : PackedScene = load(GDUtil.resolve(selected_text + ".tscn"))
 	var preview_control : GraphNode = p.instance()
 	
 	preview_control.rect_scale = Vector2(0.5, 0.5)
