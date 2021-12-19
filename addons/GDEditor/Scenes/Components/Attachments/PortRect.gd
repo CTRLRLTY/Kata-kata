49b081d70c63bbd6dbcedecefb4a92d112a8e7b5
Raznan <muhammadraznan@satpaminternet.com>
2021-12-19 12:35:07 +0700

    Refactored PortRect to be position dependant.
    PortRect will now show warning on invalid configurations

--- addons/GDEditor/Scenes/Components/Attachments/PortRect.gd
+++ addons/GDEditor/Scenes/Components/Attachments/PortRect.gd
@@ -8,85 +8,92 @@ enum PortType {
 	FLOW,
 }
 
-enum ConnectorType {
+enum {
 	NONE,
 	INPUT,
-	OUTPUT,
+	OUTPUT
 }
 
+
 export(PortType) var port_type := 0 setget set_port_type
-export(ConnectorType) var connector_type := 0 setget set_connector_type
+export(bool) var enable := false setget set_enable
+
 
 func _enter_tree() -> void:
 	if get_tree().edited_scene_root == self:
 		return
 	
 	_update_port()
-	
-	
-func get_graph_owner() -> GraphNode:
+
+
+func _get_configuration_warning() -> String:
 	var parent := get_parent()
-	assert(parent, "PortRect can't be a direct child of GraphNode")
-	var grand_parent := parent.get_parent()
-	assert(grand_parent is GraphNode, "PortRect has to be a grandchild of GraphNode")
 	
-	return grand_parent as GraphNode
+	if not parent:
+		return "PortRect has to be a child of GraphNode"
+	if parent is GraphNode:
+		return "PortRect can't be a direct child of GraphNode"
+	if not parent.get_parent() is GraphNode:
+		return "PortRect has to be a grandcild of GraphNode"
 	
+	var position := get_position_in_parent()
 	
-func get_slot() -> int:
-	var parent := get_parent()
-	assert(parent, "PortRect can't be a direct child of GraphNode")
+	if position != 0 and position != parent.get_child_count() - 1:
+		print_debug(position)
+		return "PortRect has to either be the first or last child"
 	
-	return parent.get_position_in_parent()
-	
-	
+	return ""
+
+
 func set_port_type(type : int) -> void:
 	port_type = type
 	_update_port()
-	
-	
-func set_connector_type(type : int) -> void:
-	connector_type = type
+
+
+func set_enable(p_enable : bool) -> void:
+	enable = p_enable
 	_update_port()
 	
+	
+func _graph_owner() -> GraphNode:
+	return get_parent().get_parent() as GraphNode
+
+
+func _slot() -> int:
+	return get_parent().get_position_in_parent()
+	
+	
+func _connector_type() -> int:
+	if get_position_in_parent() == 0:
+		return INPUT
+	else:
+		return OUTPUT
+
 
 func _update_port() -> void:
 	if not is_inside_tree():
 		return
-	
-	var gn := get_graph_owner()
-	
-	if not gn:
+		
+	if not _get_configuration_warning().empty():
 		return
-		
-	var slot := get_slot()
-	var input_enable := false
-	var output_enable := false
 	
-	match connector_type:
-		ConnectorType.INPUT:
-			if gn.is_slot_enabled_left(slot):
-				set_connector_type(0)
-				return
-				
-			input_enable = true
-			output_enable = gn.is_slot_enabled_right(slot)
-		ConnectorType.OUTPUT:
-			if gn.is_slot_enabled_right(slot):
-				set_connector_type(0)
-				return
-				
-			input_enable = gn.is_slot_enabled_left(slot)
-			output_enable = true
-
-	gn.set_slot(slot, 
-		input_enable, port_type, Color(1, 1, 1), 
-		output_enable, port_type, Color(1, 1, 1))
+	var gn := _graph_owner()
+	var slot := _slot()
+	
+	match _connector_type():
+		INPUT:
+			gn.set_slot_enabled_left(slot, enable)
+		OUTPUT:
+			gn.set_slot_enabled_right(slot, enable)
+	
+#	gn.set_slot(slot, 
+#			input_enable, port_type, Color(1, 1, 1), 
+#			output_enable, port_type, Color(1, 1, 1))
 		
-	# Clear up previous port from the editor.
-	gn.hide()
-	yield(get_tree(), "idle_frame")
-	gn.show()
+#	# Clear up previous port from the editor.
+#	gn.hide()
+#	yield(get_tree(), "idle_frame")
+#	gn.show()
 	
 		
 		
