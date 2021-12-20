30793426c37e80bb3bd79c336f6e64aeebb97e95
Raznan <muhammadraznan@satpaminternet.com>
2021-12-20 19:25:01 +0700

    Added tab rename on doubleclick

--- addons/GDEditor/Scenes/TabContainer.gd
+++ addons/GDEditor/Scenes/TabContainer.gd
@@ -7,12 +7,13 @@ export var TabMenuBtnPath : NodePath
 
 var tab_menu_popup : PopupMenu
 var tab_menu_btn : Button
+var tab : Tabs
 
 
 func _enter_tree() -> void:
 	tab_menu_popup = get_node(TabMenuPopupPath)
 	tab_menu_btn = get_node(TabMenuBtnPath)
-	
+	tab = get_node("Tabs")
 
 func _on_TabMenuBtn_pressed() -> void:
 	tab_menu_popup.popup(tab_menu_btn.get_global_rect())
--- addons/GDEditor/Scenes/Tabs.gd
+++ addons/GDEditor/Scenes/Tabs.gd
@@ -6,9 +6,37 @@ extends Tabs
 func _enter_tree() -> void:
 	if not get_tab_count():
 		add_tab("[empty]")
-
 		
+
+func _gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if not event.doubleclick:
+			return
+
+
+		var curr_tab_rect := get_tab_rect(current_tab)
+
+		if not curr_tab_rect.has_point(event.position):
+			return
+
+		var curr_tab_title := get_tab_title(current_tab)
+
+		# Repositioned NameEdit.
+		$NameEdit.rect_size = curr_tab_rect.size
+		$NameEdit.rect_position = get_global_rect().position + curr_tab_rect.position
+		$NameEdit.text = curr_tab_title
+		$NameEdit.caret_position = curr_tab_title.length()
+		$NameEdit.set_as_toplevel(true)
+		$NameEdit.show_modal()
+		$NameEdit.grab_focus()
+		
+		accept_event()
+
+
 func _on_tab_close(tab: int) -> void:
+	if $NameEdit.visible:
+		return
+
 	if current_tab == tab:
 		remove_tab(tab)
 	else:
@@ -22,4 +50,20 @@ func _on_tab_close(tab: int) -> void:
 func _on_TabMenuPopup_new_dialogue(dialogue_name : String) -> void:
 	add_tab(dialogue_name)
 	
-	current_tab = get_tab_count() - 1
+	# Idk wtf is going on, but if we don't wait atleast once,
+	# double clicking on a tab will select the the previous active
+	# tab. Here, I wait for two idle frames, since I still find a
+	# weird behaviour with only one idle frame yield.
+	yield(get_tree(), "idle_frame")
+	yield(get_tree(), "idle_frame")
+	
+	set_current_tab(get_tab_count() - 1)
+
+
+func _on_NameEdit_text_entered(new_text: String) -> void:
+	$NameEdit.hide()
+	set_tab_title(current_tab, new_text)
+
+
+func _on_NameEdit_hide() -> void:
+	set_tab_title(current_tab, $NameEdit.text)
