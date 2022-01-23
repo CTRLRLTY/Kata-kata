1ebdb0f8728ebf2bf354836599432b4e14e8737e
Raznan <muhammadraznan@satpaminternet.com>
2022-01-23 18:35:55 +0700

    Increment duplicated state names

--- addons/GDEditor/Scenes/ContextStateTree.gd
+++ addons/GDEditor/Scenes/ContextStateTree.gd
@@ -54,17 +54,26 @@ func _has_state_name(state_name : String) -> bool:
 
 
 func _on_button_pressed(item: TreeItem, column: int, id: int) -> void:
-	var DEFAULT_NAME = "new_variable"
+	var DEFAULT_NAME := "new_variable"
 	
 	# Add new state
 	if item == get_root():
 		var new_item = create_item(get_root())
-			
-		new_item.set_text(0, DEFAULT_NAME)
 		
 		var state := ContextStateData.new()
-		state.state_name = DEFAULT_NAME
 		
+		var state_name := DEFAULT_NAME
+		var name_suffix := ""
+		var acc := 1
+		
+		while _has_state_name(state_name + name_suffix):
+			name_suffix = "_%d" % [acc]		
+			acc += 1
+		
+		state_name += name_suffix
+		state.state_name = state_name
+		
+		new_item.set_text(0, state_name)
 		new_item.set_metadata(0, {
 				"index": _state_list.size(), "state": state})
 				
@@ -88,11 +97,14 @@ func _on_item_edited() -> void:
 	
 	state.state_name = ""
 	
-	# TODO: show a warning popup instead of a print
-	if _has_state_name(state_name):
-		print_debug("Name: '%s' is already in used" % [state_name])
-		state_name = old_name
-	
+	var name_suffix := ""
+	var acc := 1
+
+	while _has_state_name(state_name + name_suffix):
+		name_suffix = "_%d" % [acc]
+		acc += 1
+
+	state_name += name_suffix
 	state.state_name = state_name
 
 	edited.set_text(0, state_name)
