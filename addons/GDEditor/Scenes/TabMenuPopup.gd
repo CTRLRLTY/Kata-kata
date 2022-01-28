68a30531a0b994bb08df0e13dcc1b8d78d355d7c
Raznan <muhammadraznan@satpaminternet.com>
2022-01-28 18:51:32 +0700

    Added preview on/off text switch

--- addons/GDEditor/Scenes/TabMenuPopup.gd
+++ addons/GDEditor/Scenes/TabMenuPopup.gd
@@ -6,6 +6,8 @@ signal preview_dialogue
 signal new_dialogue(dialogue_name)
 signal save_dialogue
 
+var _preview_visible := false
+
 
 enum {
 	MENU_PREVIEW_DIALOGUE,
@@ -25,7 +27,7 @@ func _ready() -> void:
 	if get_item_count():
 		clear()
 	
-	add_item("Preview", MENU_PREVIEW_DIALOGUE)
+	add_item("Preview [off]", MENU_PREVIEW_DIALOGUE)
 	add_separator("", -999)
 	add_item("New Dialogue", MENU_NEW_DIALOGUE)
 	add_item("Open Dialogue", MENU_OPEN_DIALOGUE)
@@ -41,7 +43,15 @@ func _ready() -> void:
 func _on_id_pressed(id: int) -> void:
 	match id:
 		MENU_PREVIEW_DIALOGUE:
+			_preview_visible = not _preview_visible
+			
 			emit_signal("preview_dialogue")
+			
+			if _preview_visible:
+				set_item_text(MENU_PREVIEW_DIALOGUE, "Preview [on]")
+			else:
+				set_item_text(MENU_PREVIEW_DIALOGUE, "Preview [off]")
+			
 		MENU_NEW_DIALOGUE:
 			$DialogueNamePrompt.popup_centered()
 		MENU_OPEN_CHARACTER:
