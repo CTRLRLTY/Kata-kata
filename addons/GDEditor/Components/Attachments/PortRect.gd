e6730cc0328a4409881416e9779f2822c4998adf
Raznan <muhammadraznan@satpaminternet.com>
2022-02-17 19:34:52 +0700

    Disable managed port slot when its port rect is moved

--- addons/GDEditor/Components/Attachments/PortRect.gd
+++ addons/GDEditor/Components/Attachments/PortRect.gd
@@ -18,16 +18,37 @@ enum {
 export(PortType) var s_port_type := 0 setget set_port_type
 export(bool) var s_port_enable := false setget set_port_enable
 
-
+#
 func _enter_tree() -> void:
 	if get_tree().edited_scene_root == self:
 		return
 	
+	# This wait idle frame is necessary when moving the port rect position in parent.
+	# Without this, the port will update right after NOTIFICATION_UNPARENTED is called before
+	# the port rect still has not displaced. Making it wait one idle frame will ensure to the
+	# update to be called once the port rect has displaced.
+	yield(get_tree(), "idle_frame")
 	_update_port()
 
 
-func _ready() -> void:
-	_update_port()
+func _notification(what: int) -> void:
+	match what:
+		NOTIFICATION_UNPARENTED:
+			if not Engine.editor_hint:
+				return
+				
+			if not _get_configuration_warning().empty():
+				return
+				
+			var gn := _graph_owner()
+			var slot := _slot()
+			
+			match _connector_type():
+				INPUT:
+					gn.set_slot_enabled_left(slot, false)
+				OUTPUT:
+					gn.set_slot_enabled_right(slot, false)
+
 
 
 func _get_configuration_warning() -> String:
@@ -43,7 +64,6 @@ func _get_configuration_warning() -> String:
 	var position := get_position_in_parent()
 	
 	if position != 0 and position != parent.get_child_count() - 1:
-		print_debug(position)
 		return "PortRect has to either be the first or last child"
 	
 	return ""
@@ -77,7 +97,7 @@ func _connector_type() -> int:
 func _update_port() -> void:
 	if not is_inside_tree():
 		return
-		
+	
 	if not _get_configuration_warning().empty():
 		return
 	
@@ -93,6 +113,7 @@ func _update_port() -> void:
 			port_color = Color.rebeccapurple
 		PortType.FLOW:
 			port_color = Color.forestgreen
+	
 	
 	match _connector_type():
 		INPUT:
